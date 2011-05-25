83c11482eb60fd6da89de827b8c328baa11fb57d
Rasmus Sten <rasmus@dll.nu>
2011-05-25 20:45:52 +0300

    initial commit

---
+++ procsign.pl
@@ -0,0 +1,55 @@
+use Mac::Processes;
+
+my %process_signed_by;
+my %ps;
+while ( ($psn, $psi) = each(%Process) ) {
+  my $executable = $psi->processAppSpec;
+  my $pid = GetProcessPID($psn);
+  my @signdata = split /\n/, `codesign -dvvv "$executable" 2>&1`;
+
+  if (system("codesign -v $pid")) {
+    print "\t" . $executable . "\n";
+  }
+  for (@signdata) {
+    chomp;
+    my ($key, $value) = split /=/, $_, 2;
+    $ps{$psn}{'signdata'}{$key} = [ ] unless $ps{$psn}{'signdata'}{$key};
+    push @{$ps{$psn}{'signdata'}{$key}}, $value unless $value eq "";
+    #print "$key:$value --> ".$ps{$psn}{'signdata'}{$key}."\n";
+    #print "..." . join("/", @{$ps{$psn}{'signdata'}{$key}}) . "\n";
+  }
+  my $path = join('/', @{$ps{$psn}{'signdata'}{'Authority'}});
+  $ps{$psn}{'signpath'} = $path;
+  $process_signed_by{$path} = [ ] unless $process_signed_by{$path};
+  push @{$process_signed_by{$path}}, $psi;
+  #print $psi->processName . "\t" . $ps{$psn}{'signpath'} . "\n";
+}
+
+my $lastapp = "";
+my $apprepeatcount = 0;
+for (sort keys %process_signed_by) {
+  my $path = $_;
+  my @psis = @{$process_signed_by{$path}};
+  if ($path) {
+    print "\nProcesses signed by $path:\n";
+  } else {
+    print "\nUnsigned processes:\n";
+  }
+  for (@psis) {
+    my $psi = $_;
+    if ($lastapp eq $psi->processAppSpec) {
+      $apprepeatcount++;
+    } else {
+      if ($apprepeatcount > 0) {
+	print "\t (x" . $apprepeatcount . ")\n";
+      }
+      $apprepeatcount=0;
+      print "\t" . $psi->processAppSpec . " (pid " .GetProcessPID($psi->processNumber). ")\n";
+    }
+    $lastapp = $psi->processAppSpec;
+  }
+  if ($apprepeatcount > 0) {
+    print "\t (x" . $apprepeatcount . ")\n";
+    $lastapp = ""; $apprepeatcount = 0;
+  }
+}
