e6164fad7fe880b23b30068e488d83a2ad5fbd8e
Rasmus Sten <rasmus@dll.nu>
2012-06-04 23:40:03 +0300

    Enable examining of all processes.
    Don't rely on only Mac::Process; use ps to list the rest of the processes to see processes not in the user's session.
    Sort executable listings alphabetically.
    Make it possible to enable or disable examination of processes in or outside of the user session

--- procsign.pl
+++ procsign.pl
@@ -10,8 +10,8 @@
 # TODO: option to show interpreters such as Perl even with --hide-apple
 # because they may run arbitrary unsigned code
 
-use Mac::Processes;
 use Term::ANSIColor;
+use Mac::Processes;
 
 my $color = 1;
 $color = 0 if $ENV{'NOCOLOR'};
@@ -21,6 +21,8 @@ my $unsigned_in_summary = 1;
 my $warnuntrusted = 1;
 my $appleanchorcheck = 1;
 my $optc = $#ARGV + 1;
+my $stage1 = 1;
+my $stage2 = 1;
 for (@ARGV) {
     if ($_ eq "--hide-apple") {
 	push @hiddenchains, "Software Signing/Apple Code Signing Certification Authority/Apple Root CA";
@@ -38,10 +40,18 @@ for (@ARGV) {
 	$unsigned_in_summary = 0;
 	$optc--;
     }
+    if ($_ eq "--no-session") {
+	$stage1 = 0;
+	$optc--;
+    }
+    if ($_ eq "--in-session") {
+	$stage2 = 0;
+	$optc--;
+    }
 }
 if ($optc) {
     print "Usage:\n";
-    print "\t$0 [--hide-apple] [--no-color] [no-unsigned-summary]\n";
+    print "\t$0 [--hide-apple] [--no-color] [no-unsigned-summary] [--in-session]\n";
     print "\n(you may have to set the environment variable
 VERSIONER_PERL_PREFER_32_BIT=yes on an x86_64 machine)\n";
     exit 1;
@@ -54,17 +64,36 @@ my $cid = 0;
 my %process_signed_by;
 my %last_validation_failure;
 my %ps;
-print 'Examining running processes... ';
-while ( ($psn, $psi) = each(%Process) ) {
+my %pids;
+
+sub executable_from_pid {
+    my $pid = shift;
+    open LSOF, "lsof -p $pid|";
+    while (<LSOF>) {
+	my ($cmd, $pid, $user, $fd, $type, $device, $size, $node, $name) = split /[ \t]+/, $_, 9;
+	if ($fd eq "txt") {
+	    chomp $name;
+	    close LSOF;
+	    return $name;
+	}
+    }
+    close LSOF;
+    return "";
+}
+
+sub spin_wheel {
     print chr(8);
     print color $colors[$cid++] if $color;
     print $wheel[$wid++];
     $wid = 0 if ($wid > $#wheel);
     $cid = 0 if ($cid > $#colors);
     print color 'reset' if $color;
+}
 
-    my $executable = $psi->processAppSpec;
-    my $pid = GetProcessPID($psn);
+sub examineprocess {
+    my ($psn, $pid, $executable) = @_;
+    return if $pids{$pid};
+
     if ($hideapple && $appleanchorcheck) {
 	my $applecsrc = system('codesign -R="anchor apple" -v ' . $pid . ' > /dev/null 2>&1');
 	$applecsrc >>= 8;
@@ -91,16 +120,53 @@ while ( ($psn, $psi) = each(%Process) ) {
 	}
     }
     $ps{$psn}{'csrc'} = $csrc;
+    $ps{$psn}{'executable'} = $executable;
+    $ps{$psn}{'pid'} = $pid;
+    $pids{$pid} = $ps{$psn};
     my $path = "";
     $path = join('/', @{$ps{$psn}{'signdata'}{'Authority'}}) if $ps{$psn}{'signdata'}{'Authority'};
     $ps{$psn}{'signpath'} = $path;
     $last_validation_failure{$path} = $csrc;
     $process_signed_by{$path} = [ ] unless $process_signed_by{$path};
-    push @{$process_signed_by{$path}}, $psi;
+    push @{$process_signed_by{$path}}, $ps{$psn};
 }
-print chr(8) x 40;
-print ' ' x 40;
-print chr(8) x 40;
+
+if ($stage1) {
+    print 'Examining running processes... (stage 1)  ';
+    while ( ($psn, $psi) = each(%Process) ) {
+	my $executable = $psi->processAppSpec;
+	my $pid = GetProcessPID($psn);
+	spin_wheel;
+	examineprocess($psn, $pid, $executable);
+    }
+
+    print chr(8) x 42;
+    print ' ' x 42;
+    print chr(8) x 42;
+}
+
+if ($stage2) {
+    print 'Examining running processes... (stage 2)  ';
+    open PS, "ps -ax -o pid,comm|";
+    while (<PS>) {
+	s/^[ \t]+//;
+	next if /^PID/;
+	my ($pid, $cmd) = split / +/, $_, 2;
+	die "invalid pid for executable $pid" unless $pid;
+	my $executable = executable_from_pid($pid);
+	print STDERR chr(8)."\nExecutable \"$executable\" for pid $pid not found (process or file may be gone)\n" unless -f $executable;
+
+	my $psn = $pid . "_" . $executable;
+	spin_wheel;
+	examineprocess($psn, $pid, $executable);
+    }
+    close PS;
+
+    print chr(8) x 42;
+    print ' ' x 42;
+    print chr(8) x 42;
+}
+
 
 my $lastapp = "";
 my $apprepeatcount = 0;
@@ -132,29 +198,29 @@ CHAIN: for (sort keys %process_signed_by) {
 	}
 	print "Unsigned or self-signed processes:\n";
     }
-    for (@psis) {
-	my $psi = $_;
-	if ($lastapp eq $psi->processAppSpec) {
+    for (sort { $a->{'executable'} cmp $b->{'executable'} } @psis) {
+	my %p = %{$_};
+	if ($lastapp eq $p{'executable'}) {
 	    $apprepeatcount++;
 	} else {
 	    print color 'blue' if $color;
 	    if ($apprepeatcount > 0) {
-		print "\t (x" . $apprepeatcount . ")\n";
+		print "\t (x" . ($apprepeatcount+1) . ")\n";
 	    }
 	    $apprepeatcount=0;
-	    print "\t" . $psi->processAppSpec;
-	    if ($ps{$psi->processNumber}{'csrc'} == 3 && $#{$ps{$psi->processNumber}{'signdata'}{'Authority'}} == -1) {
+	    print "\t" . $p{'executable'};
+	    if ($p{'csrc'} == 3 && $#{$p{'signdata'}{'Authority'}} == -1) {
 		print color 'yellow';
 		print (" (self-signed)");
 	    }
 	    print color 'reset' if $color;
-	    print " (pid " .GetProcessPID($psi->processNumber). ")\n";
+	    print " (pid " .$p{'pid'}. ")\n";
 
 	}
-	$lastapp = $psi->processAppSpec;
+	$lastapp = $p{'executable'};
     }
     if ($apprepeatcount > 0) {
-	print "\t (x" . $apprepeatcount . ")\n";
+	print "\t (x" . ($apprepeatcount+1) . ")\n";
 	$lastapp = ""; $apprepeatcount = 0;
     }
 }
